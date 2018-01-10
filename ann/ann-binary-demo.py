307f477a43c1e93007bd90e84f9834216d0212af
sergiosaraiva <35028935+sergiosaraiva@users.noreply.github.com>
2018-01-10 10:32:41 +0000

    Update ann-binary-demo.py

--- ann/ann-binary-demo.py
+++ ann/ann-binary-demo.py
@@ -16,42 +16,47 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 # Import and prepare data
-columnMinX = 104
-columnMaxX = 126
-columnY = 127
-rowX = 3000
+columnMinIndVar = 104
+columnMaxIndVar = 126
+columnDepVar = 127
+rowBaseline = 3000
+
 originalDataSet = read_csv('BGRI2011_1106_sample.csv')
-originalX = originalDataSet.iloc[0:rowX, columnMinX:columnMaxX+1].values
-originalY = originalDataSet.iloc[0:rowX, columnY].values
+sourceIndVar = originalDataSet.iloc[0:rowBaseline, columnMinIndVar:columnMaxIndVar+1].values
+sourceDepVar = originalDataSet.iloc[0:rowBaseline, columnDepVar].values
+sourceToPredIndVar = originalDataSet.iloc[rowBaseline:, columnMinIndVar:columnMaxIndVar+1].values
 
-# Encode data if needed
-#encoderX1 = LabelEncoder()
-#originalX[:, <column to encode>] = encoderX1.fit_transform(originalX[:, <column to encode>])
+# Encode data if needed (beware dummy variable trap)
+#encoderIndVar1 = LabelEncoder()
+#sourceIndVar[:, <column to encode>] = encoderIndVar1.fit_transform(sourceIndVar[:, <column to encode>])
 #encoder = OneHotEncoder(categorical_features = [<column>])
-#originalX = encoder.fit_transform(originalX).toarray()
-#originalX = originalX[:, <column to remove>:]
+#sourceIndVar = encoder.fit_transform(sourceIndVar).toarray()
+#sourceIndVar = sourceIndVar[:, <column to remove>:]
 
-# Train and test sets
-trainX, testX, trainY, testY = train_test_split(originalX, originalY, test_size = 0.2, random_state = 0)
+# Split train, test and to predict data sets (apply feature scaling)
+trainIndVar, testIndVar, trainDepVar, testDepVar = train_test_split(sourceIndVar, sourceDepVar, test_size = 0.2, random_state = 0)
 scaler = StandardScaler()
-trainX = scaler.fit_transform(trainX)
-testX = scaler.transform(testX)
+trainIndVar = scaler.fit_transform(trainIndVar)
+testIndVar = scaler.transform(testIndVar)
+sourceToPredIndVar = scaler.transform(sourceToPredIndVar)
 
 # Create the ANN
+neurons = 12
 classifier = Sequential()
-classifier.add(Dense(units = 12, kernel_initializer = 'uniform', activation = 'relu', input_dim = 23))
-classifier.add(Dense(units = 12, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu', input_dim = 23))
+classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu'))
+#classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Train the ANN
-classifier.fit(trainX, trainY, batch_size = 16, epochs = 128)
+classifier.fit(trainIndVar, trainDepVar, batch_size = 16, epochs = 512)
+
+# Check against test set
+#testPredDepVar = classifier.predict(testIndVar)
+#testPredDepVar = (testPredDepVar > 0.5)
+#confusionMatrix = confusion_matrix(testDepVar, testPredDepVar)
 
 # Predict
-predTest = classifier.predict(testX)
-predTest = (predTest > 0.5)
-cm = confusion_matrix(testY, predTest)
-
-predX = originalDataSet.iloc[rowX:, columnMinX:columnMaxX+1].values
-predY = classifier.predict(scaler.transform(predX))
-predY = (predY > 0.5)
+predDepVar = classifier.predict(sourceToPredIndVar)
+predDepVar = (predDepVar > 0.5)
