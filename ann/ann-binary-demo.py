7224cb307a1d95f02d1f0e384f15743d8e3bd88f
sergiosaraiva <35028935+sergiosaraiva@users.noreply.github.com>
2018-01-10 00:01:18 +0000

    Update ann-binary-demo.py

--- ann/ann-binary-demo.py
+++ ann/ann-binary-demo.py
@@ -5,10 +5,9 @@ Created on Tue Jan  9 22:17:49 2018
 @author: SERGIO
 """
 
-from numpy import array
 from pandas import read_csv
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
@@ -24,13 +23,13 @@ rowX = 3000
 originalDataSet = read_csv('BGRI2011_1106_sample.csv')
 originalX = originalDataSet.iloc[0:rowX, columnMinX:columnMaxX+1].values
 originalY = originalDataSet.iloc[0:rowX, columnY].values
-predX = originalDataSet.iloc[rowX:, columnMinX:columnMaxX+1].values
 
+# Encode data if needed
 #encoderX1 = LabelEncoder()
-#originalX[:, <column>] = encoderX1.fit_transform(originalX[:, <column>])
+#originalX[:, <column to encode>] = encoderX1.fit_transform(originalX[:, <column to encode>])
 #encoder = OneHotEncoder(categorical_features = [<column>])
 #originalX = encoder.fit_transform(originalX).toarray()
-#originalX = originalX[:, 1:]
+#originalX = originalX[:, <column to remove>:]
 
 # Train and test sets
 trainX, testX, trainY, testY = train_test_split(originalX, originalY, test_size = 0.2, random_state = 0)
@@ -49,9 +48,10 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 classifier.fit(trainX, trainY, batch_size = 16, epochs = 128)
 
 # Predict
-predY = classifier.predict(testX)
-predY = (predY > 0.5)
-cm = confusion_matrix(testY, predY)
+predTest = classifier.predict(testX)
+predTest = (predTest > 0.5)
+cm = confusion_matrix(testY, predTest)
 
-predY2 = classifier.predict(scaler.transform(predX))
-predY2 = (predY2 > 0.5)
+predX = originalDataSet.iloc[rowX:, columnMinX:columnMaxX+1].values
+predY = classifier.predict(scaler.transform(predX))
+predY = (predY > 0.5)
