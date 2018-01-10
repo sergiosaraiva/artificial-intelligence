65348ebf0b29293db7a3d86fa2a5a8efee6dc64e
sergiosaraiva <35028935+sergiosaraiva@users.noreply.github.com>
2018-01-10 22:26:45 +0000

    Update ann-binary-demo.py

--- ann/ann-binary-demo.py
+++ ann/ann-binary-demo.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan  9 22:17:49 2018
-
 @author: SERGIO
 """
 
@@ -10,7 +9,7 @@ from pandas import read_csv
 #from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -22,9 +21,9 @@ columnDepVar = 127
 rowBaseline = 3000
 
 originalDataSet = read_csv('BGRI2011_1106_sample.csv')
-sourceIndVar = originalDataSet.iloc[0:rowBaseline, columnMinIndVar:columnMaxIndVar+1].values
-sourceDepVar = originalDataSet.iloc[0:rowBaseline, columnDepVar].values
-sourceToPredIndVar = originalDataSet.iloc[rowBaseline:, columnMinIndVar:columnMaxIndVar+1].values
+sourceIndVar = originalDataSet.iloc[0 : rowBaseline, columnMinIndVar : columnMaxIndVar + 1].values
+sourceDepVar = originalDataSet.iloc[0 : rowBaseline, columnDepVar].values
+sourcePredIndVar = originalDataSet.iloc[rowBaseline:, columnMinIndVar : columnMaxIndVar + 1].values 
 
 # Encode data if needed (beware dummy variable trap)
 #encoderIndVar1 = LabelEncoder()
@@ -33,17 +32,17 @@ sourceToPredIndVar = originalDataSet.iloc[rowBaseline:, columnMinIndVar:columnMa
 #sourceIndVar = encoder.fit_transform(sourceIndVar).toarray()
 #sourceIndVar = sourceIndVar[:, <column to remove>:]
 
-# Split train, test and to predict data sets (apply feature scaling)
+# Split train, test (20%) and predict data sets (apply feature scaling)
 trainIndVar, testIndVar, trainDepVar, testDepVar = train_test_split(sourceIndVar, sourceDepVar, test_size = 0.2, random_state = 0)
 scaler = StandardScaler()
 trainIndVar = scaler.fit_transform(trainIndVar)
 testIndVar = scaler.transform(testIndVar)
-sourceToPredIndVar = scaler.transform(sourceToPredIndVar)
+sourcePredIndVar = scaler.transform(sourcePredIndVar)
 
-# Create the ANN
-neurons = 12
+# Create the ANN: neurons = 1/2 independent variables (not exact science...): relu + sigmoid (last hidden layer: percentage)
+neurons = int(round(len(sourceIndVar[0]) / 2, 0))
 classifier = Sequential()
-classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu', input_dim = 23))
+classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu', input_dim = len(sourceIndVar[0])))
 classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu'))
 #classifier.add(Dense(units = neurons, kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
@@ -58,5 +57,5 @@ classifier.fit(trainIndVar, trainDepVar, batch_size = 16, epochs = 512)
 #confusionMatrix = confusion_matrix(testDepVar, testPredDepVar)
 
 # Predict
-predDepVar = classifier.predict(sourceToPredIndVar)
+predDepVar = classifier.predict(sourcePredIndVar)
 predDepVar = (predDepVar > 0.5)
