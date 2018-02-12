ecfbc095fd92c87babb8938b7b0279140ab12a9d
SERGIO\SERGIO <sergio.saraiva@gmail.com>
2018-02-12 13:44:45 +0000

    * fit code to one line instructions

--- pap-rna/rna-binary.py
+++ pap-rna/rna-binary.py
@@ -9,24 +9,19 @@ X = ds.iloc[0 : 3000, 0 : 10].values.astype(float)
 Y = ds.iloc[0 : 3000, 10].values.astype(float)
 X1 = ds.iloc[3000:, 0 : 10].values .astype(float)
 
-trainX, testX, trainY, testY = train_test_split(
-        X, Y, test_size = 0.2, random_state = 0)
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size = 0.2)
 
 scaler = StandardScaler()
 trainX = scaler.fit_transform(trainX)
 testX = scaler.transform(testX)
 
-classifier = Sequential()
-classifier.add(Dense(units = 4, kernel_initializer = 'uniform', 
-                     activation = 'relu', input_dim = 10))
-classifier.add(Dense(units = 4, kernel_initializer = 'uniform', 
-                     activation = 'relu'))
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', 
-                     activation = 'sigmoid'))
-classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', 
-                   metrics = ['accuracy'])
+rna = Sequential()
+rna.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
+rna.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu'))
+rna.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+rna.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-classifier.fit(trainX, trainY, epochs = 32)
+rna.fit(trainX, trainY, epochs = 32)
 
 X1 = scaler.transform(X1)
-predY = (classifier.predict(X1) > 0.5).astype(int)
+predY = (rna.predict(X1) > 0.5).astype(int)
