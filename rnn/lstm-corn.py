6cc611ba74c467f7651624745647a838c29b44a8
SERGIO\SERGIO <sergio.saraiva@gmail.com>
2018-02-16 01:30:32 +0000

    * refactoring

--- rnn/lstm-corn.py
+++ rnn/lstm-corn.py
@@ -6,23 +6,30 @@ Created on Tue Feb 13 20:43:50 2018
 """
 # data source: http://www.macrotrends.net/2532/corn-prices-historical-chart-data
 
-from pandas import read_csv, concat
+from pandas import read_csv
 from numpy import array, reshape
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
-dataSet = read_csv('corn-prices-historical-chart-data-train.csv')
-trainingSet = dataSet.iloc[:, 1:2].values
-#trainingSet = dataSet.iloc[32:, 1:2].values
+toPredNum = 29
+toTrainNum = 2520
+timeSteps = 128
+
+dataSet = read_csv('corn-prices-historical-chart-data.csv')
+trainingSet = dataSet.iloc[len(dataSet)-toTrainNum-toPredNum:len(dataSet)-toPredNum,1:2].values
+predSet = dataSet.iloc[len(dataSet)-(toPredNum+timeSteps):len(dataSet)+1, 1:2].values
+
+toTrainTh = len(dataSet) - (toTrainNum + toPredNum + 1)
+toPredTh = len(dataSet) - (toPredNum + 1)
 
 scaler = MinMaxScaler(feature_range = (0, 1))
 trainingSetScale = scaler.fit_transform(trainingSet)
 
 xTrain, yTrain = [], []
-for i in range(128, 2048 + 32):
-    xTrain.append(trainingSetScale[i-128:i, 0])
-    yTrain.append(trainingSetScale[i, 0])
+for i in range(timeSteps+toTrainTh,len(dataSet)-toPredNum):
+    xTrain.append(trainingSetScale[i-(timeSteps+toTrainTh):i-toTrainTh, 0])
+    yTrain.append(trainingSetScale[i-(toTrainTh+1), 0])
 xTrain, yTrain = array(xTrain), array(yTrain)
 xTrain = reshape(xTrain, (xTrain.shape[0], xTrain.shape[1], 1))
 
@@ -40,23 +47,11 @@ lstm.add(Dense(units = 1))
 lstm.compile(optimizer = 'rmsprop', loss = 'mean_squared_error')
 lstm.fit(xTrain, yTrain, epochs = 128, batch_size = 32)
 
-dataSetToPred = read_csv('corn-prices-historical-chart-data-pred.csv')
-dataSetReal = dataSetToPred.iloc[:, 1:2].values
-#dataSetReal = dataSet.iloc[0:32, 1:2].values
-#toPred = scaler.transform(dataSetReal)
-#xPred = []
-#for i in range(32, 128 + 32):
-#    xPred.append(toPred[i-128:i, 0])
-#xPred = array(xPred)
-#xPred = reshape(xPred, (xPred.shape[0], xPred.shape[1], 1))
-#yPred = scaler.inverse_transform(lstm.predict(xPred))
-
-dataSetFull = concat((dataSet['values'], dataSetToPred['values']), axis = 0)
-toPred = dataSetFull[len(dataSetFull) - len(dataSetToPred) - 128:].values
-toPred = scaler.transform(toPred.reshape(-1,1))
+predSet=predSet.reshape(-1,1)
+predSet = scaler.transform(predSet)
 xPred = []
-for i in range(128, 128 + len(dataSetToPred)):
-    xPred.append(toPred[i-128:i, 0])
+for i in range(timeSteps, timeSteps + toPredNum):
+    xPred.append(predSet[i-timeSteps:i, 0])
 xPred = array(xPred)
 xPred = reshape(xPred, (xPred.shape[0], xPred.shape[1], 1))
 yPred = scaler.inverse_transform(lstm.predict(xPred))
